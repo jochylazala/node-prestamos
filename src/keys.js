4f6e94ebe51d3cd162a1f3738169724ff8e041c1
jochylazala <62323712+jochylazala@users.noreply.github.com>
2021-04-06 10:51:33 -0400

    secon update

--- src/keys.js
+++ src/keys.js
@@ -3,6 +3,14 @@ module.exports = {
 		host: process.env.DB_HOST || 'localhost',
 		user: process.env.DB_USER || 'root',
 		password: process.env.DB_PASSWORD ||  '12345678',
-		database: process.env.DB_DATABASE || 'clientes'
+		database: process.env.DB_DATABASE || 'prestamosfym'
 	}
 };
+
+
+//mysql:
+//bf0b3e6b4a261c
+//513b4fd6
+//@
+//us-cdbr-east-03.cleardb.com
+//heroku_b792af612df21f9?reconnect=true
